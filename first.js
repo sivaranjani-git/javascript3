521937bddf92bd50f235229d1a3ad2bd1d800040
sivaranjani-git <134845313+sivaranjani-git@users.noreply.github.com>
2023-08-15 12:45:30 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,18 @@
+//boolean
+
+let num1 = 4;
+let num2 = false;
+
+console.log(num1 / num2);
+
+//null ---0
+ 
+let a=5;
+let b=null;
+console.log(a + b);
+
+//undefined
+let number=4;
+let number2 = undefined;
+console.log(number - number2);
+  
